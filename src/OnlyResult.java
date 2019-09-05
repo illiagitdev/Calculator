1ea6f0daae6993a620a4052b5f8a05ed4a73b24f
illiagitdev <illya.posv.b@gmail.com>
2019-09-05 22:02:52 +0300

    lesson2

--- src/OnlyResult.java
+++ src/OnlyResult.java
@@ -1,6 +1,17 @@
 import java.util.Scanner;
 
 public class OnlyResult {
+    static char[] hexa = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+    static public String convertString(int value, int base){
+        String result="";
+        while (value > 0) {
+            result = hexa[value % base] + result;
+            value /= base;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         int numberD;
         Scanner input = new Scanner(System.in);
@@ -13,16 +24,5 @@ public class OnlyResult {
         System.out.print("Octal number: " + convertString(numberD,8) + "\n");
 
         System.out.print("Hexadecimal number: " + convertString(numberD, 16));
-    }
-
-    static char[] hexa = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-
-    static public String convertString(int value, int base){
-        String result="";
-        while (value > 0) {
-            result = hexa[value % base] + result;
-            value /= base;
-        }
-        return result;
     }
 }

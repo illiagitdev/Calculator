e2a0704d045f4c80602902a72080c63655d29b4a
illiagitdev <illya.posv.b@gmail.com>
2019-09-16 13:15:32 +0300

    Calculator with switch

--- src/math_vectors/Calculator.java
+++ src/math_vectors/Calculator.java
@@ -3,38 +3,47 @@ package math_vectors;
 import java.util.Scanner;
 
 public class Calculator {
-    static final char[] hexa = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-
-
     public static void main(String[] args) {
         int numberD;
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Decimal number: ");
+        System.out.print("Enter a number: ");
         numberD = input.nextInt();
 
-        System.out.println("\n" + "Binary number: " + convertString(numberD, 2));
-
-        System.out.println("Octal number: " + convertString(numberD, 8));
-
-        System.out.println("Hexadecimal number: " + convertString(numberD, 16));
-
+        System.out.println("\n" + "Binary number: " + convertString(numberD, 2) +
+                "\nOctal number: " + convertString(numberD, 8) +
+                "\nHexadecimal number: " + convertString(numberD, 16));
     }
 
+    static final char[] hexa = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
     static public String convertString(int value, int base) {
         String result = "";
+        int x;
         while (value > 0) {
-            result = hexa[value % base] + result;
+            x = value % base;
+            result = hexa[x] + result;
             value /= base;
         }
-        if (base == 2) {
-            result = "0b" + result;
-        } else if (base == 8) {
-            result = "0" + result;
-        } else if (base == 16) {
-            result = "0x" + result;
+
+        switch (base) {
+            case 2: {
+                result = "0b" + result;
+                break;
+            }
+            case 8: {
+                result = "0" + result;
+                break;
+            }
+            case 16: {
+                result = "0x" + result;
+                break;
+            }
+            default: {
+                break;
+            }
         }
+
         return result;
     }
 }

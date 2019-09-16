713a9c1210f2019acc339ca3bda8dca8e56c4395
illiagitdev <illya.posv.b@gmail.com>
2019-09-16 17:52:30 +0300

    NMEA - through switch, only base information about message

--- src/tasks/MessagesReader.java
+++ src/tasks/MessagesReader.java
@@ -14,13 +14,63 @@ public class MessagesReader {
             String head = message.substring(1, message.indexOf(","));
             System.out.println(head);
 
+            String mesageType = head.substring(2);
+            System.out.println(mesageType);
 
-            String[] array = message.substring(1, message.indexOf("*")).split(",");
+            String[] array = message.substring(message.indexOf(","), message.indexOf("*")).split(",");
             showArray(array);
+
+            switch (mesageType){
+                case "GSV":{
+                    deshiferGSV(array);
+                }
+                case "GLL":{
+                    deshiferGLL(array);
+                }
+                case "RMC":{
+                    deshiferRMC(array);
+                }
+                case "VTG":{
+                    deshiferVTG(array);
+                }
+                case "GGA":{
+                    deshiferGGA(array);
+                }
+                case "GSA":{
+                    deshiferGSA(array);
+                }
+                default:{
+                    System.out.println("");
+                }
+            }
 
         } else {
             System.out.println("Not NMEA message!");
         }
+    }
+
+    private static void deshiferGSA(String[] array) {
+        System.out.println("GSA - GNSS DOP and Active Satellites");
+    }
+
+    private static void deshiferGGA(String[] array) {
+        System.out.println("GGA - Global Positioning System Fix Data");
+    }
+
+    private static void deshiferVTG(String[] array) {
+        System.out.println("VTG - Course Over Ground and Ground Speed");
+    }
+
+    private static void deshiferRMC(String[] array) {
+        System.out.println("RMC -  Recommended Minimum Specific GNSS Data");
+    }
+
+    private static void deshiferGLL(String[] array) {
+        System.out.println("GLL - Geographic Position – Latitude/Longitude");
+    }
+
+    private static void deshiferGSV(String[] array) {
+        System.out.println("GSV - GNSS DOP and Active Satellites");
     }
 
     /**

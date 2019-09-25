ea4998b698e5c1ce2cbd181cdfb405f44c4bc54e
illiagitdev <illya.posv.b@gmail.com>
2019-09-26 01:38:56 +0300

    Mode: doing something

--- src/lesson6/Mode.java
+++ src/lesson6/Mode.java
@@ -3,7 +3,7 @@ package lesson6;
 public class Mode {
     public static void main(String[] args) {
         System.out.println("\n");
-        int[] arr1 = new int[20];
+        int[] arr1 = new int[15];
         fillArrayInt(arr1);
         showArray(arr1);
 
@@ -12,27 +12,68 @@ public class Mode {
          */
         arrayMode(arr1);
 
+        //creating two linked arrays
+        //1 - contains unique values
+        //2 - contains number of unique values in the array
         shakerSort(arr1);
-        int uniqueArray=new int[countDifferent(arr1)];
         showArray(arr1);
+        int size=countDifferent(arr1);
+        System.out.println("Number of different values: "+size);
+        int[] uniqueArray = new int[size];
+        int[] uniqueCounter =new int[size];
+        int unique =separator(arr1,uniqueArray,uniqueCounter);
+        showArray(uniqueArray);
+        showArray(uniqueCounter);
 
 
         System.out.println();
         showDifferent(arr1);
     }
 
-    private static int countDifferent(int[] arr1) {
+    /**
+     * поверта два масиви: з унікальними елементами та з числом повторів цих елементів,
+     * на входатри масиви - базовий +2 і число яке вказує кількість унікальних елементів
+     */
+    private static int separator(int[] array, int[] uniqueArray, int[] uniqueCounter) {
+        int counter = 1;
+        int k=0;
+        uniqueArray[k]=array[0];
+        for (int i = 0; i < array.length - 1; i++) {
+            if (i == (array.length - 2)) {
+                System.out.println("Element " + array[array.length - 1] + " counts: " + counter);
+            }
 
+            if (array[i + 1] == array[i]) {
+                counter++;
+                continue;
+            } else {
+                uniqueCounter[k]=counter;
+                uniqueArray[++k]=array[i+1];
+                System.out.println("Element " + array[i] + " counts: " + counter);
+                counter = 1;
+            }
+
+        }
         return 0;
     }
 
-    //todo:перегрузити щоб повертав два масиви: з унікальними елементами та з числом повторів цих елементів, на входатри масиви - базовий +2 і число яке вказує кількість унікальних елементів
+    /**
+     * Works with sorted array
+     */
+    private static int countDifferent(int[] arr1) {
+        int counter = 1;
+        for (int i = 0; i < arr1.length - 1; i++) {
+            if (arr1[i]!=arr1[i+1]){
+                counter++;
+            }
+        }
+        return counter;
+    }
+
     /**
      * For sorted arrays. Виводить елемент і число його повторів у масиві
-     *
-     * @param arr1
      */
-    private static int[] showDifferent(int[] arr1) {
+    private static void showDifferent(int[] arr1) {
         int counter = 1;
         for (int i = 0; i < arr1.length - 1; i++) {
             if (arr1[i + 1] == arr1[i]) {
@@ -46,7 +87,6 @@ public class Mode {
                 System.out.println("Element " + arr1[arr1.length - 1] + " counts: " + counter);
             }
         }
-        return
     }
 
     private static void shakerSort(int[] arr1) {

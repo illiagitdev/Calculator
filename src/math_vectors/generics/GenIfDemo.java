52cde9e44e6be879ec2e35c4de1836d37bcfac94
illiagitdev <illya.posv.b@gmail.com>
2019-11-13 17:45:07 +0200

    Shild - gen interface*

--- src/math_vectors/generics/GenIfDemo.java
+++ src/math_vectors/generics/GenIfDemo.java
@@ -1,4 +1,16 @@
 package math_vectors.generics;
 
 public class GenIfDemo {
+    public static void main(String[] args) {
+        Integer[] inums={3,5,4,2,5,2,3};
+        Character[] chs={'w','c','t','q','m','a'};
+
+        MyClass<Integer> iob=new MyClass<>(inums);
+        MyClass<Character> cob=new MyClass<>(chs);
+
+        System.out.println("Max value in inums: "+iob.max());
+        System.out.println("Min value in inums: "+iob.min());
+        System.out.println("Max value in chs: "+cob.max());
+        System.out.println("Min value in chs: "+cob.min());
+    }
 }
--- src/math_vectors/generics/MinMax.java
+++ src/math_vectors/generics/MinMax.java
@@ -1,4 +1,6 @@
 package math_vectors.generics;
 
-public interface MinMax {
+public interface MinMax<T extends Comparable<T>> {
+    T min();
+    T max();
 }

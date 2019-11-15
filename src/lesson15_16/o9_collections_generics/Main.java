316e9fc7c616680dfd816ba2a09878866d4e4632
illiagitdev <illya.posv.b@gmail.com>
2019-11-15 19:32:23 +0200

    collection performance

--- src/lesson15_16/o9_collections_generics/Main.java
+++ src/lesson15_16/o9_collections_generics/Main.java
@@ -1,44 +1,15 @@
 package lesson15_16.o9_collections_generics;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Operational student = new Operational ();
         student.run();
-    }
 
 
-
-   /*
-* Задание 3
-Создать класс PerfomanceChecker
-Он должен уметь измерять время работы различных коллекций Java. А именно
-ArrayList
-LinkedList
-TreeSet
-HashSet
-
-Написать классу публичные методы для проверки:
-Скорости вставки в коллекции - checkAdding(int count)
-Функция создает пустую коллекцию, запускает таймер и начинает заполнять пустую коллекцию элементами по одному.
-* Кол-во элементов = count
-
-
-Скорости считывания с коллекции - checkReading(int count)
-Функция должна создать коллекцию, заполнить ее на count элементов. Запустить таймер и начать count раз брать рандомный
-* индекс элемента в коллекции и считывать его по индексу.
-
-
-Скорости удаления из коллекции - checkRemoving(int count)
-Функция должна создать коллекцию, заполнить ее на count элементов. Запустить таймер и начать count раз брать рандомный
-*  индекс элемента в коллекции и удалять его по индексу.
-
-Каждый метод должен замерить время работы одновременно для всех коллекций и вывести в консоль результаты указав чему
-* был равен count.
-
-Вставлять / считывать / удалять элементы в коллекциях можно только по одному. Это значит что нельзя пользоваться
-* методами removeAll, addAll.
-
-Подсказка:
-System.currentTimeMillis() - этот метод возвращает текущее время в миллисекундах
-    * */
+        List<Integer> array = new ArrayList<>();
+        List<Integer> linkedArray = new LinkedList<>();
+        Set<Integer> setList = new TreeSet<>();
+        Set<Integer> setHesh = new HashSet<>();    }
 }

28bd1ef32d8864c81354b99d83445e2532ad8bcc
illiagitdev <illya.posv.b@gmail.com>
2019-11-27 17:43:49 +0200

    concurrency*

--- src/concurrency_cookbook/Main.java
+++ src/concurrency_cookbook/Main.java
@@ -1,4 +1,13 @@
 package concurrency_cookbook;
 
+import concurrency_cookbook.one.Calculator;
+
 public class Main {
+    public static void main(String[] trees){
+        for (int i = 1; i <=10 ; i++) {
+            Calculator calculator=new Calculator(i);
+            Thread thread=new Thread(calculator);
+            thread.start();
+        }
+    }
 }

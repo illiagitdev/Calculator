602bbc328b4c5984a6a796d5f36c6bc434ee73b7
illiagitdev <illya.posv.b@gmail.com>
2019-11-28 18:04:10 +0200

    added loggin

--- src/concurrency_cookbook/one/Calculator.java
+++ src/concurrency_cookbook/one/Calculator.java
@@ -1,5 +1,8 @@
 package concurrency_cookbook.one;
 
+import logger.LogType;
+import logger.Logger;
+
 public class Calculator implements Runnable {
     private int number;
 
@@ -9,8 +12,9 @@ public class Calculator implements Runnable {
 
     @Override
     public void run() {
-        for (int i = 1; i <=10; i++) {
-            System.out.printf("%s: %d *%d = %d\n", Thread.currentThread().getName(),number,i,i*number);
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%s: %d *%d = %d\n", Thread.currentThread().getName(), number, i, i * number);
+            Logger.INSTANCE.log(LogType.SYSTEM, Thread.currentThread().getName());
         }
     }
 }

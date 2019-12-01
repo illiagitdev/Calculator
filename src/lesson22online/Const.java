d5f30b414b13c34fbf09bc48c13a1286c5427af3
illiagitdev <illya.posv.b@gmail.com>
2019-12-01 21:00:31 +0200

    lesson01-12-19*

--- src/lesson22online/Const.java
+++ src/lesson22online/Const.java
@@ -1,4 +1,10 @@
 package lesson22online;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public interface Const {
+    Map<Integer,String> MAP=new HashMap<Integer,String>(){
+        {put(2,"dfs");}
+    } ;
 }
--- src/lesson22online/Main.java
+++ src/lesson22online/Main.java
@@ -1,4 +1,64 @@
 package lesson22online;
 
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+//todo
+//lidlock
+//deadLock
+//AtomicBoolean
+//Runnable
+//Callable
+//FutureTask
+
 public class Main {
+    public static void main(String[] args) {
+//        System.out.println(Const.MAP.get(2));
+//
+//        AtomicBoolean aBoolean = new AtomicBoolean();
+//        aBoolean.get();
+//        aBoolean.set(true);
+
+        Runnable task = () -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println("Hello " + threadName);
+        };
+
+        Callable<Integer> task2 =()->{
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                return 123;
+            }catch (InterruptedException e){
+                //throw new  InterruptedException("interrupted exeption", e.printStackTrace());
+                e.printStackTrace();
+            }
+        };
+
+//        Thread thread=new Thread(task);
+//        thread.start();
+
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        service.submit(task);
+        Future<>
+        try {
+            System.out.println("try too shutdown");
+            service.shutdown();
+            service.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            if (!service.isTerminated()){
+                System.err.println("cancel non-finished task");
+            }
+            service.shutdownNow();
+            System.out.println("forsed shutdown now");
+        }
+
+        System.out.println("Done!");
+    }
+
+
+    public static void copyList(List<? extends Person> src, List<? super Person> dst) {
+        dst.addAll(src);
+    }
 }

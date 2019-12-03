fb3b60d2e0a4caf7f536ec668204ece4fc82e966
illiagitdev <illya.posv.b@gmail.com>
2019-12-03 18:20:10 +0200

    same

--- src/concurrency_cookbook/Practice.java
+++ src/concurrency_cookbook/Practice.java
@@ -1,4 +1,33 @@
 package concurrency_cookbook;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Practice {
+    public static void main(String[] args) {
+
+
+//        ExecutorService executor= Executors.newWorkStealingPool();
+//
+//        List<Callable<String>> callables= Arrays.asList(
+//                callable("task1",2);
+//        callable("task2",1);
+//        callable("task3",3);
+//    }
+//
+//    public static Callable callable(String result, long sleepSeconds){
+//        return-> {
+//            try {
+//                TimeUnit.SECONDS.sleep(sleepSeconds);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            return result;
+//        }
+//    }
+
 }

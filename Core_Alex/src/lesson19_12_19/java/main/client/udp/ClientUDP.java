08f857e8a1d9ecb5e3a3005ab4328f71d247313d
illiagitdev <illya.posv.b@gmail.com>
2019-12-20 17:14:34 +0200

    same

--- Core_Alex/src/lesson19_12_19/java/main/client/udp/ClientUDP.java
+++ Core_Alex/src/lesson19_12_19/java/main/client/udp/ClientUDP.java
@@ -1,4 +1,17 @@
 package lesson19_12_19.java.main.client.udp;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class ClientUDP {
+    private static final int PORT = 1641;
+
+    public static void main(String[] args) throws IOException {
+        DatagramSocket socket = new DatagramSocket();
+        DatagramPacket packet = new DatagramPacket(new byte[1024], 124, InetAddress.getLocalHost(), PORT);
+        packet.setData("Hello to UDP server".getBytes());
+        socket.send(packet);
+    }
 }
--- Core_Alex/src/lesson19_12_19/java/main/server/serverUDP.java
+++ Core_Alex/src/lesson19_12_19/java/main/server/serverUDP.java
@@ -1,4 +1,17 @@
 package lesson19_12_19.java.main.server;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class serverUDP {
+    private static final int PORT = 1641;
+
+    public static void main(String[] args) throws IOException {
+        DatagramSocket socket = new DatagramSocket(PORT);
+        DatagramPacket packet = new DatagramPacket(new byte[1024], 124);
+        socket.receive(packet);
+        System.out.println(new String(packet.getData()));
+        socket.close();
+    }
 }
